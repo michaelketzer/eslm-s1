55751d656f2bd0d3571abe3aa75cf08ea851ba7f
Michael Ketzer <mk@Michaels-MacBook-Pro.local>
2020-08-26 18:27:35 +0200

    Added inactive players;

--- Components/TeamPlayers.tsx
+++ Components/TeamPlayers.tsx
@@ -1,4 +1,4 @@
-import { ReactElement } from "react";
+import { ReactElement, CSSProperties } from "react";
 import { TeamStats } from "../pages";
 import { Typography, Row, Col, Card } from "antd";
 import PlayerName from "./Player/PlayerName";
@@ -11,6 +11,23 @@ interface Props {
     matches: LeagueMatch[];
 }
 
+const inactivePlayer = new Set([
+    127248003,
+    127617979,
+    313198707,
+    55649904,
+    253261807,
+    92322038,
+]);
+
+const inactiveStyle: CSSProperties = {
+    filter: 'grayscale(80%)',
+}
+
+const activeStyle: CSSProperties = {
+    boxShadow: '2px 2px 15px 0 rgba(0,0,0,.1)'
+}
+
 export default function TeamPlayers({players, playersInTeam, matches}: Props): ReactElement {
     return <>
         <Typography.Title>Spieler</Typography.Title>
@@ -18,7 +35,7 @@ export default function TeamPlayers({players, playersInTeam, matches}: Props): R
             {playersInTeam && playersInTeam.map((id) => <Col key={id} className="gutter-row" xs={24} md={12} lg={12} xl={8}>
                 <Card title={<div className={'header'}>
                     <PlayerName player={players[id]} />
-                </div>}>
+                </div>} style={inactivePlayer.has(id) ? inactiveStyle : activeStyle}>
 
                     <TopPlayerHeroes matches={matches} player={players[id]} />
                 </Card>
